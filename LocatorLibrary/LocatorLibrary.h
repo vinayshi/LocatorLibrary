3ee4b3e59bc970e06826c8b702571bde6889730a
Vinay Shivanna <vinay.shivanna@photoninfotech.net>
2017-11-06 22:08:32 +0530

    Initial Commit

---
+++ LocatorLibrary/LocatorLibrary.h
@@ -0,0 +1,19 @@
+//
+//  LocatorLibrary.h
+//  LocatorLibrary
+//
+//  Created by Vinay Shivanna on 11/6/17.
+//  Copyright © 2017 Vinay Shivanna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LocatorLibrary.
+FOUNDATION_EXPORT double LocatorLibraryVersionNumber;
+
+//! Project version string for LocatorLibrary.
+FOUNDATION_EXPORT const unsigned char LocatorLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LocatorLibrary/PublicHeader.h>
+
+
